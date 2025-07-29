594e9e451f4b857352c9ae3acf5e03ff2bf6b35b
Bram Stone <stone.bram@gmail.com>
2025-07-29 16:51:04 -0700

    change RANDOM_WALK_INTERVAL to RANDOM_WALK_INCREMENT
    re-expressed the interval between levels of discretized quantitative variables instead as the number of increments to divide them into. Better alignment with N_STATES column in the ingredients list

--- run.py
+++ run.py
@@ -374,7 +374,7 @@ def main(args):
     TRANSFER_DATA_DIR = config.get("transfer_model_dir", None)
     SEPARATE_REDOS = config.get("separate_redos", False)
     N_BAGS = config.get("n_bags", 25)
-    RANDOM_WALK_INTERVAL = config.get("transfer_model_dir", 0.1)
+    RANDOM_WALK_INCREMENT = config.get("random_walk_increment", 10)
 
     # Load the ingredients list
     if INGREDIENTS_FILE is not None:
